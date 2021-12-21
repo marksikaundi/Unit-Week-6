5243d0f54ff7f0a3e13af09d74f8dd86f16eb3fd
marksikaundi <emmanuelmark117@gmail.com>
2021-12-22 00:07:27 +0200

    learning journal added

---
+++ learning journal.py
@@ -0,0 +1,81 @@
+### Part 1
+string="Mark Sikaundi Chanda Mwila Mambwe Elija Samuel Katty Parity Ann Christabel Diana" #string with alot of words in it.
+
+print(string)
+
+list1=string.split()  #converting string to list
+
+print(list1)
+
+list1.remove("Mambwe")  #deleting
+
+print(list1)
+
+list1.pop(-1) #deleting
+
+print(list1)
+
+del list1[1] #deleting
+
+print(list1)
+
+list1.sort() #sorting
+
+print(list1)
+
+list1.append("John") #inserting
+
+print(list1)
+
+list1.insert(5,"Danny")#inserting to list
+
+print(list1)
+
+list1.extend(["Vario"])#inserting to list
+
+print(list1)
+
+list2=" ".join(list1)#converting list to string
+
+print(list2)#printing the converted string
+
+
+
+
+
+
+
+###       Part 2
+list1=["Zambia","Chile",["Algeria","Japany"]] #example of nested list
+
+print(list1)
+
+list2=["Zambia"]*2 # * operator usage
+
+print(list2)
+
+list3=list2[0:1]#uses slincing operator
+
+print(list3)
+
+list2+=["Central"]# += operator usage
+
+print(list2)
+
+list4=[True,True,False,False]
+
+list5=list(filter(None,list4)) #uses of filter
+
+print(list5)
+
+list6=["Zambia"]
+
+list6=list6.append("Central")#Central is a legal list operation which does no have an impact, instead of increasing "Cemtral" to the list, it will git the output None
+
+print(list6)
+
+
+
+## Part 3
+# The feedback i recieve from my peers, i would say this is one self experience sometimes looks unfair all that but atimes its just fair depending with what one has been doing.
+# I believe whoever i have assessed their work, they been happy i always tries to be positive and try to show some interest in someones work, than let them down.
