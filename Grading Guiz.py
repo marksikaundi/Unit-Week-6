739f35a559726eaaa7e0bb14eb697dcb1be091bc
marksikaundi <emmanuelmark117@gmail.com>
2021-12-22 20:47:24 +0200

    graded quiz

---
+++ Grading Guiz.py
@@ -0,0 +1,100 @@
+ n = 2
+n += 5
+n
+
+# pi = int(3.14159)
+
+# print(pi)
+
+
+bruce = 5
+print (bruce,)
+bruce = 7
+print (bruce)
+
+percentage = float ( 60 * 100) / 55
+print (percentage)
+
+def area(l, w):
+    temp = l * w
+    return temp
+l = 4.0
+w = 3.25
+x = area(l, w)
+if (x):
+    print (x)
+
+
+n = 10
+while n != 1:
+    print(n,)
+    if n % 2 == 0: # n is even
+        n = n / 2
+    else: # n is odd
+        n = n * 3 + 1
+
+def area(l, w):
+    temp = l * w
+    return temp
+l = 4.0
+w = 3.25
+x = area(l, w)
+if ( x ):
+    print (x)
+
+pi = float(3.14159)
+print (pi)
+
+def is_divisible(x, y):
+    
+    return (x % y) == 0
+
+def function2(param):
+    print (param, param)
+def function1(part1, part2):
+    cat = part1 + part2
+    function2(cat)
+chant1 = "See You "
+chant2 = "See Me "
+function1(chant1, chant2)
+
+x=1
+y=2
+if x == y:
+    print (x, "and", y, "are equal")
+else:
+    if x < y:
+        print (x, "is less than", y)
+    else:
+        print (x, "is greater than", y)
+
+def print_n(s, n):
+     if n > 0:
+          print(s)
+          print_n(s, n-1)
+     return n
+n = 3
+while print_n("hi", n):
+     print_n("there!", n)
+     n = 0
+
+
+x = 5
+if x % 2 == 0:
+    print (x)
+else:
+    print (x, x%2)
+
+bruce = 5
+print (bruce,)
+bruce = 7
+print (bruce)
+
+
+percentage = float ( 60 * 100) / 55
+print (percentage)
+
+
+
+
+
